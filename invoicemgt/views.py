4ffa570b82b1e99b33d1124f35e614bacb268015
Memory-Wabwile <memorywabwile@gmail.com>
2022-07-25 11:04:31 +0300

    debug

--- invoicemgt/views.py
+++ invoicemgt/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
-from django.shortcuts import render
 # Create your views here.
-from django.shortcuts import render
-from django.shortcuts import render
-# Create your views here.
+
