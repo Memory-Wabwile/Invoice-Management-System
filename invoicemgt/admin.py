feacd4c5cd8132aa791cf4c13143cfd27cade001
Memory-Wabwile <memorywabwile@gmail.com>
2022-08-13 18:08:49 +0300

    create the forms.py file

--- invoicemgt/admin.py
+++ invoicemgt/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from models import Invoice
+from .models import Invoice
 # Register your models here.
 
 admin.site.register(Invoice)
-# register the invoice model in the admin file
