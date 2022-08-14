675df969bb49015a97c6de725678eb12fd0032c2
Memory-Wabwile <memorywabwile@gmail.com>
2022-08-14 17:19:50 +0300

    create the templates folder and add home.html file

--- invoicemgt/views.py
+++ invoicemgt/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 # Create your views here.
 
+def home(request):
+    title = "welcome to home page"
+    context = {title:'title'}
+
+    return render (request , 'home.html' , context)
