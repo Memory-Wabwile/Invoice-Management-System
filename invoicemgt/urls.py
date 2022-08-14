ef3888bd50d62abe910dca30ae9ddc24dac98662
Memory-Wabwile <memorywabwile@gmail.com>
2022-08-14 18:24:31 +0300

    create the add invoice template , view and url

--- invoicemgt/urls.py
+++ invoicemgt/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
 from . import views
-from django.urls import include, path
+
 
 urlpatterns = [
-    path('', views.home , name = 'home'),
+    path('', views.home , name = "home"),
+    path('search/', views.add_invoice , name = "search"),
     
 ]
--- invoicemgt/views.py
+++ invoicemgt/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .forms import InvoiceForm
 # Create your views here.
 
 def home(request):
-    title = "welcome to home page"
-    context = {title:'title'}
+    title = "welcome to Home page"
+    context = {"title":title}
 
-    return render (request , 'home.html' , context)
+    return render (request , "home.html" , context)
+
+def add_invoice(request):
+    form = InvoiceForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {"form": form , "title": "New Invoice"}
+
+    return render (request , 'add_invoice.html' , context)
