be28ea97182b5387ae33f83ba9881cd0e844df74
Memory-Wabwile <memorywabwile@gmail.com>
2022-08-14 17:09:02 +0300

    customize the admin into a table and add a filter and search functionality

--- invoicemgt/admin.py
+++ invoicemgt/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from jmespath import search
-
 from invoicemgt.forms import InvoiceForm
 from .models import Invoice
 # Register your models here.
 
 
-class InvoiceAdmin(admin.modelAdmin):
+class InvoiceAdmin(admin.ModelAdmin):
     list_display = ['name' , 'invoice_number' , 'invoice_date']
     form = InvoiceForm
     list_filter = ['name']
     search_fields = ['name' , 'invoice_number']
+
 admin.site.register(Invoice)
