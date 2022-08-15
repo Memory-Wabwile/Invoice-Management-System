980174d8e14e1518007abfe904848ebff8ee3529
Memory-Wabwile <memorywabwile@gmail.com>
2022-08-15 14:35:34 +0300

    customize the crispy form and debug

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'invoicemgt.apps.InvoicemgtConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,5 +126,5 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
