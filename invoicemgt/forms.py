b21a3046ad4d389722a46c6920056168d6af912e
Memory-Wabwile <memorywabwile@gmail.com>
2022-08-16 11:44:34 +0300

    add validation to the update form

--- invoicemgt/forms.py
+++ invoicemgt/forms.py
@@ -31,6 +31,13 @@ class InvoiceForm(forms.ModelForm):
 			raise forms.ValidationError('This field is required')
 		return line_one
 
+	def clean_line_one_quantity(self):
+		line_one_quantity = self.cleaned_data.get('line_one_quantity')
+		if not line_one_quantity:
+			raise forms.ValidationError('This field is required')
+		return line_one_quantity
+
+		
 class InvoiceSearchForm(forms.ModelForm):
 	class Meta:
 		model = Invoice
@@ -46,3 +53,29 @@ class InvoiceUpdateForm(forms.ModelForm):
 				'line_four', 'line_four_quantity', 'line_four_unit_price', 'line_four_total_price',
 				'line_five', 'line_five_quantity', 'line_five_unit_price', 'line_five_total_price', 
 				'total', 'paid', 'invoice_type']
+
+	def clean_invoice_number(self):
+		invoice_number = self.cleaned_data.get('invoice_number')
+		if not invoice_number:
+			raise forms.ValidationError('This field is required')
+		return invoice_number
+
+	def clean_name(self):
+		name = self.cleaned_data.get('name')
+		if not name:
+			raise forms.ValidationError('This field is required')
+		return name
+
+	def clean_line_one(self):
+		line_one = self.cleaned_data.get('line_one')
+		if not line_one:
+			raise forms.ValidationError('This field is required')
+		return line_one
+
+	def clean_line_one_quantity(self):
+		line_one_quantity = self.cleaned_data.get('line_one_quantity')
+		if not line_one_quantity:
+			raise forms.ValidationError('This field is required')
+		return line_one_quantity
+
+	
